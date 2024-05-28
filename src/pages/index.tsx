60e7596780f30d8f07d2754d89b32e039ef0151b
TEZUKA, Fumihisa <tez@jastec.co.jp>
2024-05-28 01:09:41 +0000

    Ensure lesson materials

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,31 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+const Index = () => {
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
-        <div>Here comes your dreams</div>
+      <Image
+        src="/top_image.jpg"
+        alt="Image"
+        width={1920 / 3}
+        height={855 / 3}
+      />
+      <div className={styles.title}>
+        <div>Make your dreams come true from here</div>
       </div>
-      <div className={styles.center}>
-        <Link href="./aboutme">About Me</Link>
-      </div>
+      <ul>
+        <div className={styles.menuList}>
+          <Link href="./lessons/lesson1">Lesson1</Link>
+        </div>
+        <div className={styles.menuList}>
+          <Link href="./lessons/lesson2">Lesson1</Link>
+        </div>
+        <div className={styles.menuList}>
+          <Link href="./lessons/lesson3">Lesson3</Link>
+        </div>
+      </ul>
     </main>
   );
-}
+};
+export default Index;
---
+++ src/pages/lessons/lesson3.tsx
@@ -0,0 +1,14 @@
+import styles from "../page.module.css";
+import LinkToTop from "@/components/linkToTop";
+
+export default function Lesson2() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <div>３つめはだいぶ難しいかな</div>
+        {/* Hire comes your codes */}
+      </div>
+      <LinkToTop />
+    </main>
+  );
+}
